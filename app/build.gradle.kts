e140e7b64b3b2ab8b0372e511d059c5d5727e4d3
Kimi <gennady.evt@gmail.com>
2026-09-15 16:20:32 +0300

    v0.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,8 +6,8 @@ android {
         applicationId = "com.config.adblock"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.2.2"
+        versionCode = 7
+        versionName = "0.2.3"
     }
     buildTypes {
         release { isMinifyEnabled = false }
