ec0b7a96f3d622cef4840f7957568566276b8e10
Kimi <gennady.evt@gmail.com>
2026-09-15 20:21:47 +0300

    v0.4.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 15
-        versionName = "0.4.3"
+        versionName = "0.4.4"
     }
     buildTypes {
         release { isMinifyEnabled = false }
