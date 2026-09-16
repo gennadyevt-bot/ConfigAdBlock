dab9a7d7a0d556739e65ed03941a31cabf8e6519
Kimi <gennady.evt@gmail.com>
2026-09-16 21:05:12 +0300

    version 0.5.1 (build with DoH)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,8 +6,8 @@ android {
         applicationId = "com.config.adblock"
         minSdk = 26
         targetSdk = 35
-        versionCode = 16
-        versionName = "0.5.0"
+        versionCode = 17
+        versionName = "0.5.1"
     }
     signingConfigs {
         create("release") {
