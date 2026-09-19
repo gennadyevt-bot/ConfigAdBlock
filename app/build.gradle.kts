d066ef1015da4eb5d32eaeb4861fd09cebd8341c
Kimi <gennady.evt@gmail.com>
2026-09-20 01:09:12 +0300

    version 0.5.79 (89)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,8 +6,8 @@ android {
         applicationId = "com.config.adblock"
         minSdk = 26
         targetSdk = 35
-        versionCode = 88
-        versionName = "0.5.78"
+        versionCode = 89
+        versionName = "0.5.79"
     }
     signingConfigs {
         create("release") {
