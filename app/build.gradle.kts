815f4b64d128a1dec9288f3573f66bdc493d5233
Kimi <gennady.evt@gmail.com>
2026-09-25 18:03:09 +0300

    version 246 / 2.1.0-universal-beta3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,8 +6,8 @@ android {
         applicationId = "com.config.adblock"
         minSdk = 26
         targetSdk = 35
-        versionCode = 244
-        versionName = "2.1.0-universal-beta1"
+        versionCode = 246
+        versionName = "2.1.0-universal-beta3"
     }
     signingConfigs {
         create("release") {
